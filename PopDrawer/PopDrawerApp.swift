339738911f25201eb14cea39cb1cd77157bca907
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-13 20:20:45 +0200

    Initial Commit

---
+++ PopDrawer/PopDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopDrawerApp.swift
+//  PopDrawer
+//
+//  Created by Ramill Ibragimov on 13.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct PopDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
